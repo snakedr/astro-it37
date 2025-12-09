6131c69f6c6b174e81166d94586dfad03f1348a9
snakedr <100n6@pm.me>
2025-12-09 11:29:43 +0300

    mail v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,14 @@
-// astro.config.mjs
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 import path from 'path';
-import { fileURLToPath } from 'url';
-
-// Определение __dirname и __filename (нужно для path.resolve ниже)
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  // Указываем, что проект — статический (собирается в HTML/CSS/JS)
-  output: 'static',
-  
-  // Установка базового пути для всех ссылок
-  base: '/', 
-
-  // Директория для страниц и компонентов:
-  srcDir: './src', 
-
-  // ===========================================
-  // !!! КРИТИЧЕСКОЕ ИСПРАВЛЕНИЕ: НАСТРОЙКА АЛИАСА VITE !!!
-  // ===========================================
+  output: 'server', 
+  adapter: vercel(), 
   vite: {
     resolve: {
       alias: {
-        // Указываем, что @/ ссылается на папку src
-        '@/': path.resolve(__dirname, './src/')
+        '@/': path.resolve(path.resolve(), './src/')
       }
     }
   }
