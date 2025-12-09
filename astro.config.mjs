4a189f45a70721e4479d2be5f31abfb83c24ac16
snakedr <100n6@pm.me>
2025-12-09 11:33:57 +0300

    mail v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 import path from 'path';
 
 export default defineConfig({
